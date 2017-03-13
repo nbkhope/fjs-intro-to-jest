ea09a972ab4a863ab715b2571bb8a00b75b9be48
Renan Martins <martins@fujisan.co.jp>
2017-03-13 13:44:11 -0700

    Change word to words

--- utils.test.js
+++ utils.test.js
@@ -2,7 +2,7 @@ import { capitalizeAll } from './utils';
 
 describe('capitalizeAll', () => {
   test('should turn all letters of a sentence into uppercase', () => {
-    const word = 'heLLo World';
-    expect(capitalizeAll(word)).toBe('HELLO WORLD');
+    const words = 'heLLo World';
+    expect(capitalizeAll(words)).toBe('HELLO WORLD');
   });
 });
